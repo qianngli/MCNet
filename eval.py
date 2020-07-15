13851e45f1b172803b07ea1d27714ca7750db335
Qiang Li <liqmges@gmail.com>
2020-07-15 09:57:53 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -22,13 +22,16 @@ def SSIM(pred, gt):
     	
     
 def SAM(pred, gt):
-
-     
+    eps = 2.2204e-16
+    pred[np.where(pred==0)] = eps
+    gt[np.where(gt==0)] = eps 
+      
     nom = sum(pred*gt)
     denom1 = sum(pred*pred)**0.5
     denom2 = sum(gt*gt)**0.5
-    sam = np.real(np.arccos(nom.astype(np.float32)/(denom1*denom2+1e-12)))   
-    sam_sum = np.mean(np.nan_to_num(sam))*180/np.pi   	       
+    sam = np.real(np.arccos(nom.astype(np.float32)/(denom1*denom2+eps)))
+    sam[np.isnan(sam)]=0     
+    sam_sum = np.mean(sam)*180/np.pi   	       
     return  sam_sum
 
 
